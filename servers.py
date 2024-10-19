2c3218f56b6196a7e81fed3e282f4b3a7bf9a9bd
samuelhsieh0829 <66865408+samuelhsieh0829@users.noreply.github.com>
2024-10-19 19:13:02 +0800

    update

--- servers.py
+++ servers.py
@@ -4,11 +4,11 @@ server_name = "mc.ckcsc.net"
 Servers:dict[str, Server] = {}
 
 # Docker Network
-# Servers["Lobby"] = Server("mc-lobby", 25576, "Lobby")
-# Servers["Survival"] = Server("mc-survive", 25578, "Survival")
-# Servers["Activity"] = Server("mc-activity", 25577, "Activity")
+Servers["Lobby"] = Server("mc-lobby", 25576, "Lobby")
+Servers["Survival"] = Server("mc-survive", 25578, "Survival")
+Servers["Activity"] = Server("mc-activity", 25577, "Activity")
 
 # Test
-Servers["Lobby"] = Server("localhost", 25566, "Lobby")
-Servers["Activity"] = Server("localhost", 25567, "Activity")
-Servers["Survival"] = Server("localhost", 25568, "Survival")
+# Servers["Lobby"] = Server("localhost", 25566, "Lobby")
+# Servers["Activity"] = Server("localhost", 25567, "Activity")
+# Servers["Survival"] = Server("localhost", 25568, "Survival")
